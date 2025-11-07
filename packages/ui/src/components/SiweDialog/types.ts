a07c12f9d45f809681cb9bbdeaf456275900ba61
anthony2399 <anthonykhoury23991@hotmail.com>
2025-11-07 12:09:05 +0200

    feat: added SIWE modal

---
+++ packages/ui/src/components/SiweDialog/types.ts
@@ -0,0 +1,24 @@
+import { JSX } from "react";
+export interface SiweDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+
+  // Message data
+  message: string;
+  origin: string;
+  timestamp: Date;
+
+  accountAddress?: string;
+  chainName?: string;
+  chainId?: number;
+  chainIcon?: JSX.Element;
+
+  // Actions
+  onSign: () => Promise<void>;
+  onCancel: () => void;
+
+  // Status
+  isProcessing: boolean;
+  siweStatus: string;
+  canSign: boolean;
+}
