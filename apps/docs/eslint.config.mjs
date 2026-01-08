f4847733712c5f12ad98633b5587806d55d08fc3
Ghadi Mhawej <ghadimhawej@outlook.com>
2026-01-08 18:55:35 +0200

    chore: docs eslint cleanup

--- apps/docs/eslint.config.mjs
+++ apps/docs/eslint.config.mjs
@@ -1,23 +1,7 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
 import baseConfig from '../../eslint.config.mjs';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-});
 
 export default [
   ...baseConfig,
-  ...compat.config({
-    extends: ['next', 'next/core-web-vitals'],
-  }).map((config) => ({
-    ...config,
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@next/next/no-html-link-for-pages': ['error', 'apps/docs/src/app'],
-    },
-  })),
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
