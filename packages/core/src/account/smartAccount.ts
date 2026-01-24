5ef3726768017e2e7903a710d7f34559a6985667
Ghadi Mhawej <ghadimhawej@outlook.com>
2026-01-24 12:16:18 +0200

    feat: add bsc and bscTestnet to supportedNetworks

--- packages/core/src/account/smartAccount.ts
+++ packages/core/src/account/smartAccount.ts
@@ -23,7 +23,7 @@ import {
     WebAuthnAccount
 } from "viem/account-abstraction";
 import {Chain} from "../store/index.js";
-import {arbitrum, arbitrumSepolia, base, baseSepolia, linea, lineaSepolia, mainnet, optimism, optimismSepolia, sepolia , avalanche, avalancheFuji} from "viem/chains";
+import {arbitrum, arbitrumSepolia, base, baseSepolia, linea, lineaSepolia, mainnet, optimism, optimismSepolia, sepolia , avalanche, avalancheFuji, bsc, bscTestnet} from "viem/chains";
 import {PERMISSIONS_MANAGER_ADDRESS} from "../constants.js";
 import {
     getPermissionFromRelay,
@@ -64,6 +64,7 @@ export const MAINNET_CHAINS = [
     arbitrum,
     linea,
     avalanche,
+    bsc
 ]
 
 export const TESTNET_CHAINS = [
@@ -73,6 +74,7 @@ export const TESTNET_CHAINS = [
     arbitrumSepolia,
     lineaSepolia,
     avalancheFuji,
+    bscTestnet
 ]
 
 export const SUPPORTED_CHAINS = [
