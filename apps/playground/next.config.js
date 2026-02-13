569148e0e413676444a58a2366bbc5d097510697
anthony <anthonykhoury23991@hotmail.com>
2026-02-13 20:17:37 +0200

    fix: hover bg buttons

--- apps/playground/next.config.js
+++ apps/playground/next.config.js
@@ -1,5 +1,6 @@
 //@ts-check
 
+const path = require('path');
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -10,6 +11,15 @@ const nextConfig = {
   transpilePackages: ['@jaw.id/core', '@jaw.id/ui', '@jaw.id/wagmi'],
   experimental: {
     optimizePackageImports: ['lucide-react'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      wagmi: path.resolve(__dirname, 'node_modules/wagmi'),
+      '@wagmi/core': path.resolve(__dirname, '../../node_modules/@wagmi/core'),
+      '@tanstack/react-query': path.resolve(__dirname, 'node_modules/@tanstack/react-query'),
+    };
+    return config;
   },
 };
 
